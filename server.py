b0d3c2340117dfbc7d03840ee113213b609a0086
Aditi Mohanty <aditi.anomita@gmail.com>
2016-03-31 16:10:25 +0530

    Heroku

---
+++ server.py
@@ -0,0 +1,20 @@
+import os.environ
+import subprocess
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+@app.route("/RandomForest")
+def randomForest():
+	response = "I ran the random forest classifer and got"
+	retVal = subprocess.check_output(["python", "06.py"])
+	response = response +  " and accuracy of " + str(retVal) + "%"
+	return response
+
+if __name__ == "__main__":
+	port = int(os.environ.get("PORT", 5000))
+	app.run(debug=True, host='0.0.0.0', port=port)
