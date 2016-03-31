179b461f5f25a3450ef02912a5b81465508d98b6
Aditi Mohanty <aditi.anomita@gmail.com>
2016-03-31 20:57:46 +0530

    prot1

--- server.py
+++ server.py
@@ -1,47 +1,56 @@
 import os
 import subprocess
 import numpy as np
+from flask import render_template
 from sklearn import preprocessing
 from sklearn.ensemble import RandomForestClassifier
 
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 def classifier():
-	dataset = np.loadtxt("normalized.csv", delimiter=",")
 
-	train_X = dataset[1:10000,0:10]
-	train_Y = dataset[1:10000,10]
+	age = request.args.get("age")
+	gender = request.args.get("gender")
+	rest_bp = request.args.get("rest_bp")
+	fbs = request.args.get("fbs")
 
-	#print train_Y
+	print age, gender, rest_bp, fbs
 
-	#train_normalized_X = preprocessing.normalize(train_X)
+	clevelandCSV = np.loadtxt("cleveland-4.csv", delimiter=";")
 
-	#train_standardized_X = preprocessing.scale(train_X)
+	trainingFeatures = clevelandCSV[:, 0:4]
+	trainingLabels = clevelandCSV[:, 4]
+
+	testingFeatures = []
+	testingFeatures.append(float(age))
+	testingFeatures.append(float(gender))
+	testingFeatures.append(float(rest_bp))
+	testingFeatures.append(float(fbs))
+
+	print testingFeatures
+
+	testingFeatures = [testingFeatures]
+	testingFeatures_X = []
+	testingFeatures_X = np.array(testingFeatures)
+
+	testingFeatures = testingFeatures_X
+	# trainingFeatures = preprocessing.scale(trainingFeatures)
+	# testingFeatures = preprocessing.scale(testingFeatures)
 
 	model = RandomForestClassifier(n_estimators=200)
-	model.fit(train_X,train_Y)
+	model = model.fit(trainingFeatures, trainingLabels)
 
-	test_X = dataset[10000:15121,0:10]
-	test_Y = dataset[10000:15121,10]
+	predictedLabels = model.predict(testingFeatures)
 
-	predictions = model.predict(test_X)
-
-	i = 0
-	num_correct = 0
-	for prediction in predictions:
-		if(prediction== test_Y[i]):
-			num_correct += 1
-		i += 1
-
-	accuracy = float(num_correct)/float(i)
-	return (accuracy*100)
+	return int(predictedLabels[0])
 
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template('home.html')
 
 
 @app.route("/RandomForest")
@@ -49,9 +58,18 @@ def randomForest():
 	response = "I ran the random forest classifer and got"
 	# retVal = subprocess.check_output(["python", "06.py"])
 	retVal = classifier()
-	response = response +  " and accuracy of " + str(retVal) + "%"
-	return response
+	#response = response +  " and accuracy of " + str(retVal) + "%"
+	response = response + str(retVal)
+	return str(retVal)
 
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 5000))
 	app.run(debug=True, host='0.0.0.0', port=port)
+
+
+
+
+
+
+
+
