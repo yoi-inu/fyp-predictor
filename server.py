e7398be2d9bc511e3f15a7dd0c920874691ddc0a
Aditi Mohanty <aditi.anomita@gmail.com>
2016-04-06 17:54:25 +0530

    plswork

--- server.py
+++ server.py
@@ -197,17 +197,17 @@ def alertemg():
 	SMSBody = SMSBody + "," + str(longitude)
 	print "SMS Body being sent: " , SMSBody
 	retVal = "Alerted. " + SMSBody
-	# # put your own credentials here 
-	# ACCOUNT_SID = "AC7f0d7576b171275eeb549176f0a889a3" 
-	# AUTH_TOKEN = "a8cbf4b14e6453a0594256b7d3702d81" 
+	# put your own credentials here 
+	ACCOUNT_SID = "AC7f0d7576b171275eeb549176f0a889a3" 
+	AUTH_TOKEN = "a8cbf4b14e6453a0594256b7d3702d81" 
 	 
-	# client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN) 
+	client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN) 
 	 
-	# client.messages.create(
-	# 	to="+919916101013", 
-	# 	from_="+12023354404", 
-	# 	body=SMSBody,  
-	# )
+	client.messages.create(
+		to="+919916101013", 
+		from_="+12023354404", 
+		body=SMSBody,  
+	)
 
 	return retVal
 
